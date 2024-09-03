753d29d1418c85afe081689fe565ca374ca13171
ilya.drabenia <ilya.drabenia@viber.com>
2024-09-03 09:23:20 +0300

    Add bcd_inc

--- bcd_inc/bcd_inc.v
+++ bcd_inc/bcd_inc.v
@@ -1,14 +1,13 @@
 
 module bcd_inc(
     input wire [11:0] in,
-    input wire carry_in,
     output wire [11:0] out,
     output wire carry_out
 );
 
     wire [2:0] carry;
 
-    bcd_inc_4 digit_1(in[3:0], carry_in, out[3:0], carry[0]);
+    bcd_inc_4 digit_1(in[3:0], 1'h1, out[3:0], carry[0]);
     bcd_inc_4 digit_2(in[7:4], carry[0], out[7:4], carry[1]);
     bcd_inc_4 digit_3(in[11:8], carry[1], out[11:8], carry_out);
 
@@ -24,7 +23,7 @@ module bcd_inc_4(
     reg [4:0] sum;
 
     always@* begin
-        sum = in + 1 + carry_in;
+        sum = in + carry_in;
         {carry, out} = (sum > 4'h9) ? sum + 4'h6 : sum;
     end
 
--- bcd_inc/bcd_inc_tb.v
+++ bcd_inc/bcd_inc_tb.v
@@ -1,31 +1,29 @@
 module test;
     
     reg [11:0] a = 12'h000;
-    reg carry_in = 'h0;
 
     initial begin
         $dumpfile("bcd_inc_test.vcd");
         $dumpvars(0, test);
 
-        #5 carry_in = 'h0; a = 'h001;
-        #10 carry_in = 'h0; a = 'h009;
-        #15 carry_in = 'h0; a = 'h099;
-        #20 carry_in = 'h0; a = 'h999;
-        #25 carry_in = 'h1; a = 'h099;
-        #30 carry_in = 'h1; a = 'h999;
-        #35 carry_in = 'h0; a = 'h111;
-        #40 carry_in = 'h1; a = 'h889;
+        #5 a = 'h001;
+        #10 a = 'h009;
+        #15 a = 'h099;
+        #20 a = 'h999;
+        #25 a = 'h099;
+        #30 a = 'h999;
+        #35 a = 'h111;
+        #40 a = 'h889;
         
         #90 $stop;
     end
 
     wire [11:0] out;
     wire carry_out;
-    bcd_inc incr(a, carry_in, out, carry_out);
+    bcd_inc incr(a, out, carry_out);
 
     initial begin
-        $monitor("Time: %0d, a: %h, c_in: %h, out: %h, c_out: %h", 
-            $time, a, carry_in, out, carry_out);
+        $monitor("Time: %0d, a: %h, out: %h, c_out: %h", $time, a, out, carry_out);
     end
 
 endmodule
